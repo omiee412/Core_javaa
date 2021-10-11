2107bac1b5f06ce73cc6ccc08d3164c525ef01bb
omiee412 <83514446+omiee412@users.noreply.github.com>
2021-10-11 23:14:00 +0530

    Add files via upload
    Arrays

---
+++ ArrayDemo1.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner sa1 = new Scanner(System.in);
+        addition(20,5);
+    }
+
+    private static void addition(int i, int i1) {
+        int c = i + i1;
+        System.out.println("sum is " + c);
+    }
+}
