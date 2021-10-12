7ab8b593c087172bc786c0ddb7db6c6bde6d233e
omiee412 <83514446+omiee412@users.noreply.github.com>
2021-10-12 23:42:45 +0530

    Add files via upload

---
+++ ArrayDemo5.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class ArrayDemo5 {
+    public static void main(String[] args) {
+        //array literal
+        String [] courses = {"SQL","JAVA","MANUAL"};
+
+        for (int i=0; i<= courses.length; i++){
+            System.out.println(courses[i]);
+        }
+    }
+}
