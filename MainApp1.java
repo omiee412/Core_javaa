77d39080e7d7a76fc4049cb7ac8dd74fa92dfbc8
omiee412 <83514446+omiee412@users.noreply.github.com>
2022-01-29 23:44:31 +0530

    Add files via upload

--- MainApp1.java
+++ MainApp1.java
@@ -1,34 +1,8 @@
-package Overriding;
-
-import java.util.Scanner;
+package BusinessLogic;
 
 public class MainApp1 {
     public static void main(String[] args) {
-        Scanner sc1=new Scanner (System.in);
-        System.out.println("Please Select Your Bank.");
-        System.out.println(" 1: SBI ");
-        System.out.println(" 2: HDFC ");
-        System.out.println(" 3: ICICI ");
-        int choice= sc1.nextInt();
-        System.out.println("Enter Principle Amount.");
-        double p= sc1.nextDouble();
-        System.out.println("Enter Time Span in Years.");
-        double t= sc1.nextDouble();
-
-        if (choice==1){
-            SBI s1=new SBI();
-            double r=0.04;
-            s1.simpleInterest(p, r, t);
-        }else if (choice==2){
-            HDFC h1=new HDFC ();
-            double r=0.05;
-            h1.simpleInterest(p, r, t);
-        }else if (choice==3){
-            ICICI i1=new ICICI ();
-            double r=0.06;
-            i1.simpleInterest(p, r, t);
-        }else
-            System.out.println("Inavlid Choice.");
-
+        System.out.println("k value is "+Master1.k);
+        Master1.test();
     }
 }
---
+++ Master1.java
@@ -0,0 +1,8 @@
+package BusinessLogic;
+
+public class Master1 { //Business logic class or Functional class
+    static int k=20;
+    static void test(){
+        System.out.println("Test Method");
+    }
+}
